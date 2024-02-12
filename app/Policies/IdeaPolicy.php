31cd1f27bc1d5f5e214e9f9fab15e34ee4736a69
Wahni Adnani (DEF) <adnaniwahni@gmail.com>
2024-02-13 02:02:25 +0800

    user authorization

--- app/Policies/IdeaPolicy.php
+++ app/Policies/IdeaPolicy.php
@@ -15,7 +15,7 @@ class IdeaPolicy
     public function update(User $user, Idea $idea): bool
     {
         // edit /update
-        return ($user->is_admin || $user->id === $idea->user_id);
+        return ($user->is_admin || $user->is($idea->user));
     }
 
     /**
@@ -23,7 +23,8 @@ class IdeaPolicy
      */
     public function delete(User $user, Idea $idea): bool
     {
-        return ($user->is_admin || $user->id === $idea->user_id);
+        // return ($user->is_admin || $user->id === $idea->user_id);
+        return ($user->is_admin ||  $user->is($idea->user));
     }
 
 
