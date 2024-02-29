72e3cb55719f08787ae2bd9f9e33f75d1ac00ed3
Wahni Adnani (DEF) <adnaniwahni@gmail.com>
2024-02-29 22:14:15 +0800

    admin widgets

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,7 +1,9 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Comment;
+use App\Models\Idea;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
@@ -12,7 +14,9 @@ class DashboardController extends Controller
 
     public function index(){
 
-
-        return view("admin.dashboard");
+        $totalUsers = User::count();
+        $totalIdeas = Idea::count();
+        $totalComments = Comment::count();
+        return view("admin.dashboard", compact("totalUsers","totalIdeas","totalComments"));
     }
 }
